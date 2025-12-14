615b5458b65cf65101437b3b5d86566e8cdbe516
ASHWIN <email@example.com>
2025-12-14 11:38:57 +0530

    Loading Data with torchvision.datasets

---
+++ PYT4.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+    def __len__(self):
+        return len(self.data)
+    
+    def ___getitem__(self, idx):
+        sample = self.data[idx]
+        label = self.labels[idx]
+        if self.transform:
+            sample = self.transform(sample)
+            return sample, label
